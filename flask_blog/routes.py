49051f5ecf35053d9f20c40b537c7017a6c125f1
Michael Park <michaelpark@Michaels-MacBook-Pro.local>
2019-02-10 19:43:58 +0000

    uploading of images locally

--- flask_blog/routes.py
+++ flask_blog/routes.py
@@ -1,7 +1,13 @@
 from flask import render_template, request, redirect, url_for
 from flask_blog import app, mongo
 from bson.objectid import ObjectId
+from flask_uploads import UploadSet, configure_uploads, IMAGES
 
+
+photos = UploadSet('photos', IMAGES)
+
+app.config['UPLOADED_PHOTOS_DEST'] = 'flask_blog/static/img'
+configure_uploads(app, photos)
 
 @app.route("/")
 def index():
@@ -13,17 +19,34 @@ def view_all():
     sessions = mongo.db.study_session.find()
     return render_template('view_all.html', sessions = sessions)
 
+def save_picture_to_s3(form_picture):          
+    random_hex = ''.join([random.choice(string.digits) for n in range(8)])    
+    _, f_ext = os.path.splitext(form_picture.filename)  
+    picture_fn = random_hex + f_ext
+    s3 = boto3.resource('s3')
+    s3.Bucket('mpark-flask-training-calendar').put_object(Key="static/workout_pics/" + picture_fn, Body=form_picture)
+    return picture_fn 
+
 @app.route("/add_a_session", methods=["POST", "GET"])
 def add_a_session():
+    form_values = request.form.to_dict()
     if request.method == "POST":
-        form_values = request.form.to_dict()
-        mongo.db.study_session.insert_one(form_values)
+        for photo in request.files:
+            filename = "img/" + photos.save(request.files[photo])
+            for k,v in form_values.items():
+                if k.startswith(photo):
+                    form_values[k]=filename
+                    nk = k.split().pop()
+                    " ".join(nk)
+                    form_values[nk] = form_values.pop(k)
         print(form_values)
-        return "Post route"
+        # mongo.db.study_session.insert_one(form_values)
+        return "post route"
     else:
         return render_template("add_a_session.html")
 
 @app.route("/view_detail/<session_id>")
 def view_detail(session_id):
     session = mongo.db.study_session.find_one({"_id": ObjectId(session_id)})
+    print(session)
     return render_template("view_detail.html", session=session)
--- flask_blog/static/js/script.js
+++ flask_blog/static/js/script.js
@@ -3,7 +3,11 @@ let num = 2
 function add_field(type) {
     console.log('clicked')
     num += 1
-    $("#study_form").append("<div id='input_group" + num + "'><p><input id='key" + num + "' type='text' required><input id='value" + num + "' type='" + type + "' required> <span onclick='delete_inputs(" + num + ")'>X<span></p><input type='hidden' id='field_to_be_submitted" + num + "'></div>");
+    if(type=='file'){
+        $("#study_form").append("<div id='input_group" + num + "'><p><input id='key" + num + "' type='text' required><input id='value" + num + "' type='" + type + "' name='photo" + num + "' required> <span onclick='delete_inputs(" + num + ")'>X<span></p><input type='hidden' id='field_to_be_submitted" + num + "'></div>");
+    } else {
+        $("#study_form").append("<div id='input_group" + num + "'><p><input id='key" + num + "' type='text' required><input id='value" + num + "' type='" + type + "' required> <span onclick='delete_inputs(" + num + ")'>X<span></p><input type='hidden' id='field_to_be_submitted" + num + "'></div>");
+    }
 }
 
 function delete_inputs(num) {
@@ -17,7 +21,11 @@ function delete_inputs(num) {
 function changeName() {
     for (i = 0; i <= num; i++) {
         if (document.getElementById('input_group' + i)) {
-            document.getElementById(`field_to_be_submitted${i}`).setAttribute("name", document.getElementById(`key${i}`).value)
+            if(document.getElementById(`value${i}`).type=='file'){
+                document.getElementById(`field_to_be_submitted${i}`).setAttribute("name", "photo" + i + " " + document.getElementById(`key${i}`).value)
+            } else {
+                document.getElementById(`field_to_be_submitted${i}`).setAttribute("name", document.getElementById(`key${i}`).value)
+            }
             document.getElementById(`field_to_be_submitted${i}`).setAttribute("value", document.getElementById(`value${i}`).value)
             console.log('testing')
         } else {
