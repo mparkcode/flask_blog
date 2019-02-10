3b7551835e1294c9f2b573ac53012a0bf7f0dd47
Michael Park <michaelpark@Michaels-MacBook-Pro.local>
2019-02-10 17:13:20 +0000

    Initial s3 setup

--- flask_blog/__init__.py
+++ flask_blog/__init__.py
@@ -1,11 +1,14 @@
 from flask import Flask
 import os
 from flask_pymongo import PyMongo
+from flask_s3 import FlaskS3
 
 
 app = Flask(__name__)
 app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.config["FLASKS3_BUCKET_NAME"] = os.environ.get("AWS_STORAGE_BUCKET_NAME")
 mongo = PyMongo(app)
+s3 = FlaskS3(app)
 
 from flask_blog import routes
